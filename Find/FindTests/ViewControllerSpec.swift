6a9d548efd4821d7a52af3ea16e8a27a243eb4a9
Justin.Dombecki <Justin.Dombecki@gmail.com>
2015-02-12 19:13:22 -0500

    Refactor.

--- Find/FindTests/ViewControllerSpec.swift
+++ Find/FindTests/ViewControllerSpec.swift
@@ -4,19 +4,11 @@ import Nimble
 class ViewControllerSpec: QuickSpec {
     override func spec() {
         describe("the application's view controller", {
-
-            
-            context("Creation", {
-                it("should be created from the Main storyboard", {
+            context("when created from storyboard", {
+                it("should be the inital view controller of the 'Main' storyboard - found from its restoration id", {
                     let storyboard = UIStoryboard(name: "Main", bundle: NSBundle(forClass: ViewController.self))
                     let viewController = storyboard.instantiateInitialViewController() as UIViewController
                     expect(viewController.restorationIdentifier).to(equal("ViewController"))
-                })
-            })
-            
-            context("Outlets", {
-                it("should ", {
-                    
                 })
             })
         })
