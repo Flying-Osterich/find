dd283fbb83657d1b6206bac785003450b85a99ee
Justin.Dombecki <Justin.Dombecki@gmail.com>
2015-02-18 00:26:05 -0500

    KIF works with swift. The ability to write functional tests in Swift is achieved.

---
+++ Find/FindFunctionalTests/KIF+SwiftExtension.swift
@@ -0,0 +1,21 @@
+//
+//  KIF+SwiftExtension.swift
+//  Find
+//
+//  Created by Justin.Dombecki on 2/18/15.
+//  Copyright (c) 2015 Justin.Dombecki. All rights reserved.
+//
+
+extension XCTestCase {
+    
+    var tester: KIFUITestActor { return tester() }
+    var system: KIFSystemTestActor { return system() }
+    
+    private func tester(_ file : String = __FILE__, _ line : Int = __LINE__) -> KIFUITestActor {
+        return KIFUITestActor(inFile: file, atLine: line, delegate: self)
+    }
+    
+    private func system(_ file : String = __FILE__, _ line : Int = __LINE__) -> KIFSystemTestActor {
+        return KIFSystemTestActor(inFile: file, atLine: line, delegate: self)
+    }
+}
