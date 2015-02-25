f4ddc53a344cf22215b2513e5b71644fcd5e7129
Justin.Dombecki <Justin.Dombecki@gmail.com>
2015-02-24 22:12:03 -0500

    NSRange conforms to Equatable. (NSRange == NSRange) ?

---
+++ Find/Find/Tools.swift
@@ -0,0 +1,17 @@
+//
+//  Tools.swift
+//  Find
+//
+//  Created by Justin.Dombecki on 2/24/15.
+//  Copyright (c) 2015 Justin.Dombecki. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: Equatable
+
+extension NSRange : Equatable { }
+public func ==(lhs: NSRange, rhs: NSRange) -> Bool {
+    return lhs.location == rhs.location && lhs.length == rhs.length
+}
