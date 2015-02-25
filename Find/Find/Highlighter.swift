dad39d6985b38f3fcecac1fb3ec270a9d78af87c
Justin.Dombecki <Justin.Dombecki@gmail.com>
2015-02-24 22:28:53 -0500

    A Highlighter will highlight a string within another string by coloring the background color of its range Yellow.

---
+++ Find/Find/Highlighter.swift
@@ -0,0 +1,23 @@
+//
+//  Highlighter.swift
+//  Find
+//
+//  Created by Justin.Dombecki on 2/24/15.
+//  Copyright (c) 2015 Justin.Dombecki. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Highlighter {
+    func highlight(string:String, inString:String) -> NSAttributedString {
+        let mutableAttibutedString = NSMutableAttributedString(string: inString)
+        
+        let ranges = Finder().find(string, inString: inString)
+        for range in ranges {
+            mutableAttibutedString.addAttribute(NSBackgroundColorAttributeName, value: UIColor.yellowColor(), range: range)
+        }
+        
+        return mutableAttibutedString.copy() as NSAttributedString
+    }
+}
---
+++ Find/FindTests/HighlighterSpec.swift
@@ -0,0 +1,28 @@
+import Quick
+import Nimble
+
+class HighlighterSpec: QuickSpec {
+    override func spec() {
+        describe("a Highlighter") {
+            it("should change the backgroundColor to highlight a string within a string") {
+                
+                let string = "a String"
+                let enclosingString = "there is a String in this string"
+                let rangeOfString = NSMakeRange(9, 8)
+                
+                let highlighter = Highlighter()
+                let attributedEnclosedString = highlighter.highlight(string, inString:enclosingString)
+
+                var hasAttributes = false
+                attributedEnclosedString.enumerateAttribute(NSBackgroundColorAttributeName, inRange: NSMakeRange(0, attributedEnclosedString.length), options: .LongestEffectiveRangeNotRequired) { (var value, var range, stop) in
+                    hasAttributes = true
+                    if range == rangeOfString  {
+                        expect(value).notTo(beNil())
+                        expect(value as? NSObject).to(beAKindOf(UIColor))
+                    }
+                }
+                expect(hasAttributes).to(beTrue())
+            }
+        }
+    }
+}
