7bd1c60973851668e15f3f3ce682bcc4dd492af5
Justin.Dombecki <Justin.Dombecki@gmail.com>
2015-03-24 20:58:32 -0400

    Update unit test specs to match new Quick syntax.

--- Find/FindTests/ParserSpec.swift
+++ Find/FindTests/ParserSpec.swift
@@ -5,14 +5,14 @@ class ParserSpec: QuickSpec {
     override func spec() {
         describe("a Parser") {
             context("for HTML") {
-                it("should find all the plain text in a html document", {
+                it("should find all the plain text in a html document") {
                     let fileOpener = FileOpener()
                     if let htmlDoc = fileOpener.open("sun.html") {
                         let parser = Parser()
                         let plainText = parser.parseHtmlDoc(htmlDoc)
                         println("\(plainText)")
                     }
-                })
+                }
             }
         }
     }
--- Find/FindTests/ToolsSpec.swift
+++ Find/FindTests/ToolsSpec.swift
@@ -4,14 +4,14 @@ import Nimble
 class ToolsSpec: QuickSpec {
     override func spec() {
         describe("Equatables") {
-            it("should match NSRange", {
+            it("should match NSRange") {
                 let range1 = NSMakeRange(0, 0)
                 let range2 = NSMakeRange(0, 0)
                 let range3 = NSMakeRange(2, 2)
                 
                 expect(range1).to(equal(range2))
                 expect(range1).notTo(equal(range3))
-            })
+            }
         }
     }
 }
