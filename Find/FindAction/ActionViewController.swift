671421b07cdb0a3e8b8b2181bcdaebc9acd49be8
Justin.Dombecki <Justin.Dombecki@gmail.com>
2015-03-31 22:35:07 -0400

    ActionViewController displays an UISearchBar with keyboard when viewDidLoad. Extension dismissed when SearchButtonClicked.

--- Find/FindAction/ActionViewController.swift
+++ Find/FindAction/ActionViewController.swift
@@ -9,11 +9,28 @@
 import UIKit
 import MobileCoreServices
 
-class ActionViewController: UIViewController {
+class ActionViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var searchBarBottomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        consumeExtensionContent()
+        observeKeyboardNotifications()
+
+        self.searchBar.becomeFirstResponder()
+        self.searchBar.delegate = self
+    }
     
+// MARK : UISearchBarDelegate
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        exportExtensionContent()
+    }
+    
+// MARK: Extension
+    func consumeExtensionContent() {
         for item: AnyObject in self.extensionContext!.inputItems {
             println("\(item)")
             let inputItem = item as! NSExtensionItem
@@ -26,16 +43,36 @@ class ActionViewController: UIViewController {
         }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func done() {
+    func exportExtensionContent() {
         let extensionItem = NSExtensionItem()
         extensionItem.attachments = [NSItemProvider(item: [NSExtensionJavaScriptFinalizeArgumentKey:["bgColor":"red"]],
-                                          typeIdentifier: kUTTypePropertyList as String)]
+            typeIdentifier: kUTTypePropertyList as String)]
         self.extensionContext!.completeRequestReturningItems([extensionItem], completionHandler: nil)
     }
-
+    
+// MARK: Keyboard
+    
+    func observeKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name:UIKeyboardWillShowNotification, object: nil);
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name:UIKeyboardWillHideNotification, object: nil);
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self);
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        var info = notification.userInfo!
+        var keyboardFrame: CGRect = (info[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        
+        UIView.animateWithDuration(0.1, animations: { () -> Void in
+            self.searchBarBottomConstraint.constant = keyboardFrame.size.height
+        })
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        UIView.animateWithDuration(0.1, animations: { () -> Void in
+            self.searchBarBottomConstraint.constant = 0
+        })
+    }
 }
