36bb6bbb4950cfbe444da9bc83910a584e223919
Justin.Dombecki <Justin.Dombecki@gmail.com>
2015-04-19 11:16:18 -0400

    In-app keyboard interactions are more desired.

--- Find/Find/ViewController.swift
+++ Find/Find/ViewController.swift
@@ -12,10 +12,11 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        observeKeyboardNotifications()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,5 +29,48 @@ class ViewController: UIViewController {
         textView.attributedText = attributedString
     }
 
+    @IBAction func backgroundTapped(sender: UITapGestureRecognizer) {
+        textView.resignFirstResponder()
+        textField.resignFirstResponder()
+    }
+    
+    // MARK: Keyboard
+    
+    func observeKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name:UIKeyboardWillHideNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardDidShow:"), name:UIKeyboardDidShowNotification, object: nil)
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self);
+    }
+
+    func keyboardWillHide(notification: NSNotification) {
+        bottomConstraint.constant = 0.0
+        self.scrollToCursor()
+    }
+    
+    func keyboardDidShow(notification: NSNotification) {
+        let (endFrame, duration) = self.endFrameAndDurationFromKeyboardNotification(notification)
+        UIView.animateWithDuration(duration) { () -> Void in
+            self.bottomConstraint.constant = endFrame.height
+        }
+        self.scrollToCursor()
+    }
+    
+    func endFrameAndDurationFromKeyboardNotification(notification: NSNotification) -> (CGRect, NSTimeInterval) {
+        var info = notification.userInfo!
+        let keyboardEndFrameValue = info[UIKeyboardFrameEndUserInfoKey] as! NSValue
+        let keyboardAnimationDuration = info[UIKeyboardAnimationDurationUserInfoKey] as! NSTimeInterval
+        return (keyboardEndFrameValue.CGRectValue(), keyboardAnimationDuration)
+    }
+
+    func scrollToCursor () {
+        // toggle scroll enabled,
+        // shouldn't have to do this
+        textView.scrollEnabled = false;
+        textView.scrollRangeToVisible(textView.selectedRange)
+        textView.scrollEnabled = true;
+    }
 }
 
